764bc1646e4c7805963fae4c25970675adf849a5
CHOI JEEMIN <choijeemin@CHOIui-MacBook-Pro.local>
2017-10-27 01:08:09 +0900

    fetch

--- XAICWebProject/settings.py
+++ XAICWebProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bbsr996kpht6_di$945m@mlzz^-^k6%xux*cv++gxy2ej7)q6r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
